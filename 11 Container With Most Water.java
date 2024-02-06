2f03dcdb7ea4dde7c439e0748c0912db894b45b1
Ashwak11 <120675155+Ashwak11@users.noreply.github.com>
2024-02-06 14:07:00 +0530

    Update 11 Container With Most Water.java

--- 11 Container With Most Water.java
+++ 11 Container With Most Water.java
@@ -29,24 +29,23 @@ n == height.length
   */
 
 class Solution {
-public:
-    int maxArea(vector<int>& height) {
+    public int maxArea(int[] height) {
         int left = 0;
-        int right = height.size() - 1;
-        int maxArea = 0;
-
-        while (left < right) {
-            int currentArea = min(height[left], height[right]) * (right - left);
-            maxArea = max(maxArea, currentArea);
-
-            if (height[left] < height[right]) {
+        int right = height.length - 1;
+        int max = 0;
+        while(left < right){
+            int w = right - left;
+            int h = Math.min(height[left], height[right]);
+            int area = h * w;
+            max = Math.max(max, area);
+            if(height[left] < height[right]) left++;
+            else if(height[left] > height[right]) right--;
+            else {
                 left++;
-            } else {
                 right--;
             }
         }
-
-        return maxArea;
-        
+        return max;
     }
-};
+}
+
